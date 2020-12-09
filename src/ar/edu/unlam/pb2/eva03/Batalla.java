e57bbb1f254012e33a3cff410863fa00fc2c07f3
gastonachaval <grodriguezachaval@alumno.unlam.edu.ar>
2020-12-09 10:18:22 -0300

    7

--- src/ar/edu/unlam/pb2/eva03/Batalla.java
+++ src/ar/edu/unlam/pb2/eva03/Batalla.java
@@ -4,10 +4,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import ar.edu.unlam.pb2.eva03.enumeradores.TipoDeBatalla;
-import ar.edu.unlam.pb2.eva03.excepciones.VehiculoIncompatible;
-import ar.edu.unlam.pb2.eva03.interfaces.Acuatico;
-import ar.edu.unlam.pb2.eva03.interfaces.Terrestre;
-import ar.edu.unlam.pb2.eva03.interfaces.Volador;
 
 public class Batalla {
 	private Double latitud;
